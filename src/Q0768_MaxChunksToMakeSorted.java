b1f9f1420c3555432088aa2440f9dedfe96cba38
alwithy <zxcvgvbnm@vip.qq.com>
2020-04-21 22:21:34 +0900

    add solution for Q0768

---
+++ src/Q0768_MaxChunksToMakeSorted.java
@@ -0,0 +1,36 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q0768_MaxChunksToMakeSorted {
+    public int maxChunksToSorted(int[] arr) {
+        if (arr.length == 1) {
+            return 1;
+        }
+        //遍历数组，将数组划分为左右两块
+        //当左块最大值小于右块最小值，则可以划分为两块
+        //左侧用大根堆，右侧用小根堆
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new MyComparator());
+        int res = 1;
+        for (int i = 0; i < arr.length; i++) {
+            minHeap.add(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            maxHeap.add(arr[i]);
+            minHeap.remove(arr[i]);
+            if (!minHeap.isEmpty() && minHeap.peek() >= maxHeap.peek()) {
+                res++;
+                maxHeap = new PriorityQueue<>(new MyComparator());
+            }
+        }
+        return res;
+    }
+
+    //实现大根堆的比较器
+    static class MyComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return b - a;
+        }
+    }
+}
