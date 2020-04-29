fa5b49b859120a89e57ab17325a9db55d6fb70e1
alwithy <zxcvgvbnm@vip.qq.com>
2020-04-29 13:05:31 +0900

    add solution for Q0039

---
+++ src/Q0039_CombinationSum.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Q0039_CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length < 1) {
+            return res;
+        }
+        Arrays.sort(candidates);
+        if (candidates[0] > target) {
+            return res;
+        }
+
+        dfs(candidates, target, 0, new ArrayDeque<>(), res);
+        return res;
+    }
+
+    /**
+     * @param candidates 目标数组
+     * @param residue 剩余值
+     * @param begin 本次搜索的起点
+     * @param path 本次遍历中使用到的元素
+     * @param res 解集
+     */
+    private void dfs(int[] candidates,
+                     int residue,
+                     int begin,
+                     Deque<Integer> path,
+                     List<List<Integer>> res) {
+        if (residue == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < candidates.length; i++) {
+            //数组已经经过排序，所以剪枝
+            //若residue减去当前candidates[i]小于0,则之后的元素不可能满足条件
+            if (residue < candidates[i]) {
+                break;
+            }
+
+            path.addLast(candidates[i]);
+            dfs(candidates, residue - candidates[i], i, path, res);
+            path.removeLast();
+        }
+    }
+}
