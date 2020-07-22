ea31c3c0607678771659c352086ea1f63bb28fda
alwithy <zxcvgvbnm@vip.qq.com>
2020-07-22 22:03:56 +0900

    add solution for Q0513

---
+++ src/Q0513_FindLargestValueInEachTreeRow.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q0513_FindLargestValueInEachTreeRow {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        // 层序遍历
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Deque<TreeNode> curLine = new LinkedList<>();
+        Deque<TreeNode> nextLine;
+        curLine.add(root);
+        int max;
+        while (!curLine.isEmpty()) {
+            nextLine = new LinkedList<>();
+            max = Integer.MIN_VALUE;
+            while (!curLine.isEmpty()) {
+                root = curLine.poll();
+                max = Math.max(max, root.val);
+
+                if (root.left != null) {
+                    nextLine.add(root.left);
+                }
+                if (root.right != null) {
+                    nextLine.add(root.right);
+                }
+            }
+
+            res.add(max);
+            curLine = nextLine;
+        }
+
+        return res;
+    }
+}
