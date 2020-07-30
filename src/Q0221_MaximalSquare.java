cfc13797337e1a66953814dec8e320f4139ac1f1
alwithy <zxcvgvbnm@vip.qq.com>
2020-07-30 22:06:53 +0900

    add solution for Q0221

---
+++ src/Q0221_MaximalSquare.java
@@ -0,0 +1,26 @@
+public class Q0221_MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        // 动态规划,dp[i][j]代表以[i][j]为右下角的最大正方形的边长
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+}
