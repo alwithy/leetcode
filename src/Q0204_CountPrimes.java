ba5f2eb08c50039d2c51b5e01c90ad2ecde9d719
alwithy <zxcvgvbnm@vip.qq.com>
2020-12-05 11:36:34 +0900

    add solution for Q0204

---
+++ src/Q0204_CountPrimes.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Q0204_CountPrimes {
+    public int countPrimes(int n) {
+        int[] isPrime = new int[n];
+        Arrays.fill(isPrime, 1);
+        int res = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i] == 1) {
+                res++;
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = 0;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
