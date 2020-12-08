e49cbf75fecb25e955572ef5ad1d89ff15b7d249
alwithy <zxcvgvbnm@vip.qq.com>
2020-12-08 17:04:38 +0900

    improve solution for Q0842

--- src/Q0842_SplitArrayIntoFibonacciSequence.java
+++ src/Q0842_SplitArrayIntoFibonacciSequence.java
@@ -1,87 +1,86 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Q0842_SplitArrayIntoFibonacciSequence {
     public static List<Integer> splitIntoFibonacci(String S) {
-        List<List<Integer>> res = new ArrayList<>();
-        // dfs解决
-        List<Integer> path = new ArrayList<>();
-        // 挑选前两个数字
-        // 第一个数字S[0, i],第二个数字S[i, j]
+        //dfs,首先挑选前两个数字
+        List<Integer> path = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        int len = S.length();
+        //第一个数字为S[0,i],第二个数字为S[i + 1,j]
+        long first = 0, second = 0;
         if (S.charAt(0) != '0') {
-            for (int i = 1; i <= (S.length() - 1)/2 + 1; i++) {
-                long first = Long.parseLong(S.substring(0, i));
+            out:
+            for (int i = 0; i < len/2 + 1; i++) {
+                first = first * 10 + S.charAt(i) - '0';
                 if (first > Integer.MAX_VALUE) break;
                 path.add((int) first);
-                if (S.charAt(i) != '0') {
-                    for (int j = i + 1; j <= (S.length() + 1)/2 + 1; j++) {
-                        long second = Long.parseLong(S.substring(i, j));
+                if (S.charAt(i + 1) != 0) {
+                    second = 0;
+                    for (int j = i + 1; j < len - 1; j++) {
+                        second = second * 10 + S.charAt(j) - '0';
                         if (second > Integer.MAX_VALUE) break;
                         path.add((int) second);
-                        dfs(S, j, path, res);
-                        if (res.size() > 0) break;
+                        dfs(S, j + 1, path, res);
+                        if (!res.isEmpty()) break out;
                         path.remove(1);
                     }
-                    if (res.size() > 0) break;
                 } else {
                     path.add(0);
-                    dfs(S, i + 1, path, res);
-                    if (res.size() > 0) break;
+                    dfs(S, i + 2, path, res);
+                    if (!res.isEmpty()) break;
                     path.remove(1);
                 }
-
                 path.remove(0);
             }
         } else {
-            path = new ArrayList<>();
             path.add(0);
             if (S.charAt(1) != '0') {
-                for (int i = 2; i <= (S.length() + 1)/2 + 1; i++) {
-                    long second = Long.parseLong(S.substring(1, i));
+                for (int j = 1; j < len; j++) {
+                    second = second * 10 + S.charAt(j) - '0';
                     if (second > Integer.MAX_VALUE) break;
                     path.add((int) second);
-                    dfs(S, i, path, res);
-                    if (res.size() > 0) break;
+                    dfs(S, j + 1, path, res);
+                    if (!res.isEmpty()) break;
                     path.remove(1);
                 }
             } else {
                 path.add(0);
                 dfs(S, 2, path, res);
+                path.remove(1);
             }
             path.remove(0);
         }
 
-        return res.size() > 0 ? res.get(0) : new ArrayList<>();
+        return res.isEmpty() ? new LinkedList<>() : res.get(0);
     }
 
     /**
      *
-     * @param s 本次搜索用到的字符串
-     * @param begin 本次搜索的起点
-     * @param path 本次搜索已包含的数字
-     * @param res 符合条件的解
+     * @param S 使用的字符串
+     * @param index 本次搜索开始的位置
+     * @param path 已经保存的数列
+     * @param res  结果集
      */
-    private static void dfs(String s,
-                     int begin,
-                     List<Integer> path,
-                     List<List<Integer>> res) {
-        if (res.size() > 0) return;
-        if (begin == s.length()) {
-            res.add(new ArrayList<>(path));
+    public static void dfs(String S, int index, List<Integer> path, List<List<Integer>> res) {
+        if (index == S.length()) {
+            res.add(new LinkedList(path));
             return;
         }
 
-
-        int target = path.get(path.size() - 1) + path.get(path.size() - 2);
-        if (s.charAt(begin) == '0' && target > 0) return;
-        long cur = -1;
-        for (int i = begin + 1; i <= s.length() && cur < target; i++) {
-            cur = Long.parseLong(s.substring(begin, i));
-            if (cur > Integer.MAX_VALUE) break;
-            if (cur == target) {
+        int size = path.size();
+        int target = path.get(size - 1) + path.get(size - 2);
+        long cur = 0;
+        for (int i = index; i < S.length(); i++) {
+            cur = cur * 10 + S.charAt(i) - '0';
+            if (cur == target && (i + 1 == S.length() || S.charAt(i + 1) != 0)) {
                 path.add((int) cur);
-                dfs(s, i, path, res);
-                path.remove(path.size() - 1);
+                dfs(S, i + 1, path, res);
+                if (!res.isEmpty()) break;
+                path.remove(size);
+            } else if (cur > target) {
+                break;
             }
         }
     }
