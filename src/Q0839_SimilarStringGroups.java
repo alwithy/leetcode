f02913145b048836e575633d7ea78c569f73118d
alwithy <zxcvgvbnm@vip.qq.com>
2021-01-31 14:52:14 +0900

    add solution for Q0839

---
+++ src/Q0839_SimilarStringGroups.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+
+public class Q0839_SimilarStringGroups {
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!uf.isConnected(i, j)) {
+                    if (isSimilar(strs[i], strs[j])) {
+                        uf.union(i, j);
+                    }
+                }
+            }
+        }
+
+        return n - uf.count;
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+            }
+            if (count > 2) {
+                break;
+            }
+        }
+
+        return count <= 2;
+    }
+
+    static class UnionFind {
+        int[] parent;
+        int[] rank;
+        int count;//合并成功的次数
+
+        public UnionFind(int n) {
+            rank = new int[n];
+            Arrays.fill(rank, 1);
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (rank[rootX] > rank[rootY]) {
+                int tmp = rootX;
+                rootX = rootY;
+                rootY = tmp;
+            }
+
+            parent[rootX] = rootY;
+            rank[rootY] += rank[rootX];
+            count++;
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
+}
